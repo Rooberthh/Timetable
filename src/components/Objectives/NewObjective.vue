6af39e4751d958280f59dd6841a2026c8bbea978
rooberthh <roberth.evaldsson@hotmail.com>
2020-01-11 22:11:33 +0100

    taskDetails UI

--- src/components/Objectives/NewObjective.vue
+++ src/components/Objectives/NewObjective.vue
@@ -1,10 +1,35 @@
 <template>
-    
+    <div class="p-2 mb-3">
+        <form @submit.prevent="addObjective">
+            <input type="text" placeholder="Add a new Objective..." class="w-full leading-loose" name="body" v-model="body">
+        </form>
+    </div>
 </template>
 
 <script>
+    import axios from 'axios';
+
     export default {
-        name: "NewObjective"
+        props: ['endpoint'],
+        data() {
+            return {
+                body: '',
+                completed: false
+            }
+        },
+        methods: {
+            addObjective() {
+                axios.post(this.endpoint, {body: this.body})
+                    .catch(error => {
+                        flash(error.message);
+                    })
+                    .then(response => {
+                        this.body = '';
+
+                        this.$emit('created', response.data)
+                    });
+            }
+        }
     }
 </script>
 
