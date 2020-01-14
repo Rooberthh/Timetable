9ab7f22c685e63603502783f90f84366a40126cb
rooberthh <roberth.evaldsson@hotmail.com>
2020-01-14 18:09:14 +0100

    basic home UI

--- src/components/StatisticsCard.vue
+++ src/components/StatisticsCard.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="sm:w-1/2 md:w-1/3 px-3 pb-6">
+    <div class="sm:w-1/2 md:w-1/4 px-3 pb-6">
         <div class="bg-white flex justify-center py-5 px-3 shadow-md">
             <div class="card-body">
                 <h3 class="font-bold text-5xl text-center" v-if="count !== null"> {{ count }}</h3>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,28 +3,50 @@
     <div class="flex flex-wrap -mx-3">
       <statisticsCard
         :url="bookUrl"
-        text="Amount of books"
+        text="Books read"
       />
-      <statisticsCard
-        :url="activityUrl"
-        text="Amount of Activities"
-      />
+    </div>
+    <h2 class="text-3xl">Tasks</h2>
+    <div class="flex flex-wrap -mx-3">
+      <div class="sm:w-1/2 md:w-1/4 px-3 pb-6" v-for="status in statuses" :key="status.id">
+        <div class="bg-white flex justify-center py-5 px-3 shadow-md">
+          <div class="card-body">
+            <h3 class="font-bold text-5xl text-center"> {{ status.tasks.length }}</h3>
+            <p class="text-grey-700 text-lg text-center">Tasks in <span class="font-bold">{{ status.name }}</span></p>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import statisticsCard from '../components/StatisticsCard';
+import axios from 'axios';
 export default {
 
   name: 'home',
   components: {
     'statisticsCard': statisticsCard
   },
+  created() {
+    this.fetch();
+  },
   data() {
     return {
       bookUrl: this.getGatewayUrl() + "books",
-      activityUrl: this.getGatewayUrl() + "activities"
+      statuses: [],
+    }
+  },
+  methods: {
+    fetch() {
+      axios.get(this.getGatewayUrl() + 'statuses')
+        .then(response => {
+          this.statuses = response.data;
+        })
+        .catch(error => {
+          flash(error.message);
+        });
     }
   }
 
